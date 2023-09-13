dd1afab5eca9c1d7ba1104aaaee9a6806ebb3229
Clément URGEN <curgen@consultants-solutec.fr>
2023-09-13 17:50:56 +0200

    deuxieme jour

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -10,30 +10,6 @@ export class AppComponent implements OnInit {
   faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.faceSnaps = [
-      new FaceSnap(
-        'Photo',
-        'Ma super photo',
-        new Date(),
-        60,
-        'https://yt3.googleusercontent.com/ytc/AOPolaQxGi77lBm0y9SlTT8TkqsgqxSNSLdh6Be6OiyL=s900-c-k-c0x00ffffff-no-rj',
-        'Paris'
-      ),
-      new FaceSnap(
-        'Autre Photo',
-        'Mon autre super photo',
-        new Date(),
-        160,
-        'https://i.pinimg.com/originals/10/ba/4a/10ba4afc650ce14862f6447e2730b9a8.png'
-      ),
-      new FaceSnap(
-        'Dernière Photo',
-        'Ma dernière super photo',
-        new Date(),
-        240,
-        'https://www.nautiljon.com/images/perso/00/22/chouji_akimichi_622.webp',
-        'Konoha'
-      ),
-    ];
+    
   }
 }
